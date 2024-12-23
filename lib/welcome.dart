ff9e08aefdcba17c0751b2ac68a260f3d643530f
Siwakorn <s6504062610277@email.kmutnb.ac.th>
2024-12-23 11:24:41 +0700

    StatelessStatefulTest

---
+++ lib/welcome.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  String _output = '';
+  final _textController = TextEditingController();
+  final _numberController = TextEditingController();
+  final _timeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Welcome',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: IconButton(
+              onPressed: () {
+                debugPrint('leading menu');
+              },
+              icon: const Icon(Icons.menu)),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter your name',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _numberController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Clothes',
+                    hintText: 'Service price = clothes * 10',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _timeController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Time(Minutes)',
+                    hintText: 'Service price = minutes / 60 * 0.8',
+                  ),
+                ),
+              ),
+              const Text(
+                'welcome',
+                style: TextStyle(fontSize: 18),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  String _input = _textController.text;
+                  String _inputnum = _numberController.text;
+                  String _inputtime = _timeController.text;
+
+                  double? num = double.parse(_inputnum);
+                  double? time = double.parse(_inputtime);
+                  setState(() {
+                    _output = 'Service is ${num * time * 10 / 60 * 0.8} baht';
+                  });
+                  debugPrint(
+                      'Hi .. $_input , Your clothes are $num , $_output');
+                },
+                child: Text('Calculate'),
+              ),
+              Text('$_output'),
+            ],
+          ),
+        ));
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projectweek4/main.dart';
+import 'package:week4_widget/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
